0adfe22d112183517869b11b007ec92764ecf90d
ismaelnaciri <114733902+ismaelnaciri@users.noreply.github.com>
2024-03-21 11:58:40 +0100

    Everything works except ex1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ClientSideComponentComponent } from './client-side-component/client-side-component.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientSideComponentComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/serverIsmaelNaciriFernandez.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const cors = require('cors');
+
+app.use(cors(), express.json());
+port = 3030;
+
+app.listen(port, () => {
+  console.log("Server listening to port ::" + port)
+})
+
+//Ex1
+const imagesDirPath = __dirname + "\\assets\\UF1_ExamenAaDIsmaelNaciriFernandez\\Imatges";
+app.get("/llegirImatgesNaciriFernandez", async (req, res) => {
+  const files = fs.readdirSync(imagesDirPath);
+  files.forEach((file) => {
+    const readableStream = fs.createReadStream(path.join(imagesDirPath, file), {highWaterMark: 4096}); //highWaterMark = amount of chunks per wave
+
+    readableStream.on('data', (chunk) => {
+      console.log("Image: " + file);
+
+      const buffer = Buffer.from(chunk);
+      console.log(buffer);
+    });
+  });
+});
+
+//Ex2
+app.get("/mostraNomsNaciriFernandez", (req, res) => {
+
+  function readDirectory(dir) {
+    const files = fs.readdirSync(dir);
+    files.forEach((file) => {
+      const path = dir + `\\` + file;
+      const stats = fs.statSync(path);
+      if (stats.isDirectory()) {
+        console.log("Directory: " + path);
+        readDirectory(path);
+      } else if (stats.isFile()) {
+        console.log("File: " + path);
+      }
+    })
+  }
+
+
+  const absoluteRoute = "C:\\IdeaProjects\\2nDAM\\AAD Recuperacio\\UF1\\UF1ExIsmaelNaciriFernandez\\src\\assets\\UF1_ExamenAaDIsmaelNaciriFernandez";
+  console.log("TEST WITH " + absoluteRoute + "\n")
+  readDirectory(absoluteRoute);
+
+  const relativeRoute = __dirname + "\\assets\\UF1_ExamenAaDIsmaelNaciriFernandez";
+  console.log("\n\nTEST WITH " + "\\assets\\UF1_ExamenAaDIsmaelNaciriFernandez" + "\n");
+  readDirectory(relativeRoute);
+});
+
+
+//Ex3
+app.put("/writeBuffersNaciriFernandez", (req, res) => {
+  if (fs.existsSync(__dirname + "\\assets\\UF1_ExamenAaDIsmaelNaciriFernandez\\Documents\\Buffers") === false) {
+    console.log("Heello")
+    fs.mkdirSync("..\\src\\assets\\UF1_ExamenAaDIsmaelNaciriFernandez\\Documents\\Buffers", {recursive: true});
+  }
+  const writeStream = fs.createWriteStream(__dirname + "\\assets\\UF1_ExamenAaDIsmaelNaciriFernandez\\Documents\\Buffers\\ex4IsmaelNaciriFernandez.txt", {
+    flags: "a+",
+    encoding: "utf8"
+  });
+
+  const nameBuffer = Buffer.from("Ismael\n");
+  writeStream.write(nameBuffer);
+  const surnameBuffer = Buffer.from("Naciri\n");
+  writeStream.write(surnameBuffer);
+  const secondSurnameBuffer = Buffer.from("Fernandez\n");
+  writeStream.write(secondSurnameBuffer);
+
+  writeStream.write("Dijous 21/03/2024");
+
+  console.log("Writing done!!");
+});
+
+//Ex4
+app.post("/copiaFitxerNaciriFernandez", (req, res) => {
+  const readPath = __dirname + "\\assets\\UF1_ExamenAaDIsmaelNaciriFernandez\\Documents\\FitxerOrigen.txt"
+  const writePath = __dirname + "\\assets\\UF1_ExamenAaDIsmaelNaciriFernandez\\Documents\\Docs1\\FitxerDesti.txt";
+  fs.readFile(readPath, 'utf8', (err, data) => {
+    if (err) {
+      console.log("Error reading the file  |  " + err);
+    }
+
+    if (writePath) {
+      fs.writeFile(writePath, data, (err) => {
+        if (err) {
+          console.log("Error writing to file  ||  " + err);
+        } else {
+          console.log(`El contingut del fitxer ${path.basename(readPath)} s'ha COPIAT correctament en el fitxer ${path.basename(writePath)}`);
+        }
+      })
+    } else {
+      //Here you can use either writeFileSync and flag 'a+', or with fs.appendFile
+      //Both append the tedxt given to the file provided, if the file does not exists, it creates it
+      fs.appendFile(writePath, data, (err) => {
+        if (err) {
+          console.log("Error appending or creating the file  ||  " + err);
+        } else {
+          console.log(`El contingut del fitxer ${path.basename(readPath)} s'ha CONCATENAT correctament en el fitxer ${path.basename(writePath)}`)
+        }
+      })
+    }
+  })
+});
